745958a16ea4257d3fae8e80f445ad69ff3c7bc3
Nanco-L <khlee1992@naver.com>
2017-03-26 15:33:45 +0900

    double layer lstm tmp commit

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import tensorflow as tf
 from loadMusic.musicXMLio import musicXMLtoArray
+from rnnModel.LSTMDoubleLayer import lstm_double
 
 test_arr = musicXMLtoArray('..\\Summer.xml')
 
-print(test_arr[0][240])
+lowest_note = 24
+highest_note = 102
+note_span = highest_note - lowest_note
+
+my_lstm = lstm_double(note_span*2)
+
+x, seq, out = my_lstm.get_dynamicRNN(1)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    outval = out.eval(feed_dict={seq: [len(test_arr[1])],
+                                 x: test_arr[1].reshape([len(test_arr[1]), 1, note_span*2])})
+
+print(outval)
---
+++ rnnModel/LSTMDoubleLayer.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+import numpy as np
+
+
+class lstm_double:
+    def __init__(self, input_dim):
+        self.input_dim = input_dim
+        self.first_cell = tf.contrib.rnn.LSTMCell(input_dim)
+        self.second_cell = tf.contrib.rnn.LSTMCell(input_dim)
+
+    def get_dynamicRNN(self, batch_size):
+        first_zero_state = self.first_cell.zero_state(batch_size, tf.float32)
+        batch_x = tf.placeholder(tf.float32, [None, batch_size, self.input_dim])
+        seq_length = tf.placeholder(tf.int32, [batch_size])
+
+        first_out, _ = tf.nn.dynamic_rnn(self.first_cell,
+                                         batch_x,
+                                         sequence_length=seq_length,
+                                         initial_state=first_zero_state,
+                                         time_major=True,
+                                         scope='first_layer')
+
+        second_zero_state = self.second_cell.zero_state(batch_size, tf.float32)
+
+        second_out, _ = tf.nn.dynamic_rnn(self.second_cell,
+                                          first_out,
+                                          sequence_length=seq_length,
+                                          initial_state=second_zero_state,
+                                          time_major=True,
+                                          scope='second_layer')
+
+        return batch_x, seq_length, second_out
