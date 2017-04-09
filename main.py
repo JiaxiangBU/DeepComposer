f48b55ea93d311029fcf75ff84695dea944298b1
Nanco-L <khlee1992@naver.com>
2017-04-09 22:46:39 +0900

    training process stable

--- main.py
+++ main.py
@@ -3,24 +3,29 @@ from loadMusic.musicXMLio import musicXMLtoArray
 from rnnModel.LSTMDoubleLayer import lstm_double
 
 test_arr = musicXMLtoArray('..\\Summer.xml')
+print(test_arr[1].shape)
 
 lowest_note = 24
 highest_note = 102
 note_span = highest_note - lowest_note
 
 my_lstm = lstm_double(note_span*2)
-print(my_lstm.first_cell)
-print(my_lstm.second_cell)
+#print(my_lstm.first_cell)
+#print(my_lstm.second_cell)
 
-x, seq, out = my_lstm.get_dynamicRNN(1)
+my_lstm.get_dynamicRNN(1)
+my_lstm.setTrainer()
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    outval = out.eval(feed_dict={seq: [len(test_arr[1])],
-                                 x: test_arr[1].reshape([len(test_arr[1]), 1, note_span*2])})
+    for i in range(100):
+        my_lstm.optim.run(feed_dict={my_lstm.seq_length: [len(test_arr[1])-1],
+                                     my_lstm.batch_x: test_arr[1][:-1].reshape([len(test_arr[1])-1, 1, note_span * 2]),
+                                     my_lstm.batch_y: test_arr[1][1:].reshape([len(test_arr[1])-1, 1, note_span * 2])})
+        if i%5 == 0:
+            loss = my_lstm.loss.eval(feed_dict={my_lstm.seq_length: [len(test_arr[1])-1],
+                                                my_lstm.batch_x: test_arr[1][:-1].reshape([len(test_arr[1])-1, 1, note_span * 2]),
+                                                my_lstm.batch_y: test_arr[1][1:].reshape([len(test_arr[1])-1, 1, note_span * 2])})
+            print(loss)
 
-    first_state = my_lstm.first_cell.zero_state(1, tf.float32)
-    second_state = my_lstm.second_cell.zero_state(1, tf.float32)
-    start_x, _, _, pred = my_lstm.LSTM_step(first_state, second_state)
-
-print(outval)
+#print(outval)
--- rnnModel/LSTMDoubleLayer.py
+++ rnnModel/LSTMDoubleLayer.py
@@ -10,26 +10,27 @@ class lstm_double:
 
     def get_dynamicRNN(self, batch_size):
         first_zero_state = self.first_cell.zero_state(batch_size, tf.float32)
-        batch_x = tf.placeholder(tf.float32, [None, batch_size, self.input_dim])
-        seq_length = tf.placeholder(tf.int32, [batch_size])
+        self.batch_x = tf.placeholder(tf.float32, [None, batch_size, self.input_dim])
+        self.batch_y = tf.placeholder(tf.float32, [None, batch_size, self.input_dim])
+        self.seq_length = tf.placeholder(tf.int32, [batch_size])
 
         first_out, _ = tf.nn.dynamic_rnn(self.first_cell,
-                                         batch_x,
-                                         sequence_length=seq_length,
+                                         self.batch_x,
+                                         sequence_length=self.seq_length,
                                          initial_state=first_zero_state,
                                          time_major=True,
                                          scope='first_layer')
 
         second_zero_state = self.second_cell.zero_state(batch_size, tf.float32)
 
-        second_out, _ = tf.nn.dynamic_rnn(self.second_cell,
-                                          first_out,
-                                          sequence_length=seq_length,
-                                          initial_state=second_zero_state,
-                                          time_major=True,
-                                          scope='second_layer')
+        self.second_out, _ = tf.nn.dynamic_rnn(self.second_cell,
+                                               first_out,
+                                               sequence_length=self.seq_length,
+                                               initial_state=second_zero_state,
+                                               time_major=True,
+                                               scope='second_layer')
 
-        return batch_x, seq_length, second_out
+        #return batch_x, seq_length, second_out
 
     def LSTM_step(self, first_state, second_state):
         #first_state = self.first_cell.zero_state(batch_size, tf.float32)
@@ -43,4 +44,9 @@ class lstm_double:
             out, second_state = self.second_cell(out, second_state)
 
         return inputs, first_state, second_state, out
+
+    def setTrainer(self):
+        self.loss = tf.reduce_mean(tf.square(self.second_out - self.batch_y))
+        self.optim = tf.train.AdamOptimizer().minimize(self.loss)
+
 
